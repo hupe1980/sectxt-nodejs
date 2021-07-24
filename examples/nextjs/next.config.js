cdd235c2ef8e71b3a2428be763dfe418a1bb274f
hupe1980 <frankhuebner1980@gmail.com>
2021-07-24 22:59:53 +0200

    fix nextjs

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -3,9 +3,14 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/.well-known/security.txt',
-        destination: '/api/sectxt',
+        source: "/.well-known/security.txt",
+        destination: "/api/sectxt",
       },
-    ]
+    ];
   },
-}
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
